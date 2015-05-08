29c54fbed339914bb4de02a9ee17067fbe04f5e4
Hyacinthe Cartiaux <Hyacinthe.Cartiaux@uni.lu>
2015-05-08 15:01:50 +0200

    update content of 'params.pp'
    Signed-off-by: Hyacinthe Cartiaux <Hyacinthe.Cartiaux@uni.lu>

--- tests/params.pp
+++ tests/params.pp
@@ -15,12 +15,33 @@
 
 include 'backupninja::params'
 
-$names = ['ensure', 'protocol', 'port', 'packagename']
+$names = ["ensure", "loglevel", "reportemail", "reportsuccess", "reportinfo", "reportwarning", "reportspace", "reporthost", "reportuser", "reportdirectory", "admingroup", "logfile", "configdirectory", "scriptdirectory", "usecolors", "when", "vservers", "libdirectory", "packagename", "configfile", "configfile_mode", "taskfile_mode", "lvmnetbackup_mode", "configfile_owner", "configfile_group"]
 
 notice("backupninja::params::ensure = ${backupninja::params::ensure}")
-notice("backupninja::params::protocol = ${backupninja::params::protocol}")
-notice("backupninja::params::port = ${backupninja::params::port}")
+notice("backupninja::params::loglevel = ${backupninja::params::loglevel}")
+notice("backupninja::params::reportemail = ${backupninja::params::reportemail}")
+notice("backupninja::params::reportsuccess = ${backupninja::params::reportsuccess}")
+notice("backupninja::params::reportinfo = ${backupninja::params::reportinfo}")
+notice("backupninja::params::reportwarning = ${backupninja::params::reportwarning}")
+notice("backupninja::params::reportspace = ${backupninja::params::reportspace}")
+notice("backupninja::params::reporthost = ${backupninja::params::reporthost}")
+notice("backupninja::params::reportuser = ${backupninja::params::reportuser}")
+notice("backupninja::params::reportdirectory = ${backupninja::params::reportdirectory}")
+notice("backupninja::params::admingroup = ${backupninja::params::admingroup}")
+notice("backupninja::params::logfile = ${backupninja::params::logfile}")
+notice("backupninja::params::configdirectory = ${backupninja::params::configdirectory}")
+notice("backupninja::params::scriptdirectory = ${backupninja::params::scriptdirectory}")
+notice("backupninja::params::usecolors = ${backupninja::params::usecolors}")
+notice("backupninja::params::when = ${backupninja::params::when}")
+notice("backupninja::params::vservers = ${backupninja::params::vservers}")
+notice("backupninja::params::libdirectory = ${backupninja::params::libdirectory}")
 notice("backupninja::params::packagename = ${backupninja::params::packagename}")
+notice("backupninja::params::configfile = ${backupninja::params::configfile}")
+notice("backupninja::params::configfile_mode = ${backupninja::params::configfile_mode}")
+notice("backupninja::params::taskfile_mode = ${backupninja::params::taskfile_mode}")
+notice("backupninja::params::lvmnetbackup_mode = ${backupninja::params::lvmnetbackup_mode}")
+notice("backupninja::params::configfile_owner = ${backupninja::params::configfile_owner}")
+notice("backupninja::params::configfile_group = ${backupninja::params::configfile_group}")
 
 #each($names) |$v| {
 #    $var = "backupninja::params::${v}"
