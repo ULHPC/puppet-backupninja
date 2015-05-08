c7c92ff76311b44ac6d77d9c840a8ea9193d44b4
Hyacinthe Cartiaux <Hyacinthe.Cartiaux@uni.lu>
2015-05-08 15:01:27 +0200

    manifests reorganization

---
+++ manifests/common/redhat.pp
@@ -0,0 +1,10 @@
+# File::      <tt>backupninja.pp</tt>
+# Author::    Hyacinthe Cartiaux (hyacinthe.cartiaux@uni.lu)
+# Copyright:: Copyright (c) 2012 Hyacinthe Cartiaux
+# License::   GPLv3
+#
+# ------------------------------------------------------------------------------
+# = Class: backupninja::common::redhat
+#
+# Specialization class for Redhat systems
+class backupninja::common::redhat inherits backupninja::common { }
